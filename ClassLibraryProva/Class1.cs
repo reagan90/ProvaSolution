b0c7efd05deb1ba320bad90c29f53e2b0def00ab
rbpelenda <rbpelenda@gmail.com>
2021-03-22 22:18:03 +0100

    Aggiungere i file di progetto.

---
+++ ClassLibraryProva/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryProva
+{
+    public class Class1
+    {
+    }
+}
